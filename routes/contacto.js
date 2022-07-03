70d9e67898a2e468728c648949d24ca6320722f9
Lucía Puppo <lu.puppo.91@gmail.com>
2022-07-03 15:47:49 -0300

    funcionamiento envío de mail

--- routes/contacto.js
+++ routes/contacto.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+
 
 router.get('/', function (req, res, next) {
     res.render('contacto', {
@@ -7,5 +9,37 @@ router.get('/', function (req, res, next) {
     });
 })
 
+//capturando los datos. De alguna forma necesito
+//tener los datos que ingresa el usuario
+router.post('/', async function (req, res, next) {
+    console.log(req.body)
+    var nombre = req.body.nombre;
+    var email = req.body.email;
+    var tel = req.body.tel;
+    var comentarios = req.body.comentarios;
 
-module.exports = router;
+    var obj = {
+        to: 'lu.puppo.91@gmail.com',
+        subject: 'Contacto desde la pagina web',
+        html: nombre + " se contacto a través de la web y quiere saber más información a este correo: " + email + ".<br> Su tel es: " + tel + ". Su comentario es: " + comentarios + "."
+    }
+
+    var transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS
+    }
+});
+
+var info = await transport.sendMail(obj);
+
+res.render('contacto',{
+    message: 'Mensaje enviado correctamente',
+    isContacto: true
+})
+
+});
+
+module.exports = router; //finaliza router.com
